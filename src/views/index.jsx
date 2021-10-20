1f723e51a1b1eac50d01abd94bd00a40ecc61866
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-19 20:25:13 -0400

    Archivos iniciales del proyecto

---
+++ src/views/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Index extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <h1>Welcome to Unlimited Images</h1>
+            </React.Fragment>
+        )
+    }
+}   
+
+export default Index;
