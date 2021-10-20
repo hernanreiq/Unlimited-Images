01e90d9502e575b30a23b837829d22c6404dcbef
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-20 03:12:05 -0400

    Agregado NavLink de react-router-dom al Navbar

--- src/views/partials/header.jsx
+++ src/views/partials/header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
     render() {
@@ -7,13 +8,13 @@ class Header extends Component {
             <React.Fragment>
                 <Navbar bg="success" expand="lg" variant="dark">
                     <Container>
-                        <Navbar.Brand href="/" className="text-white font-weight-bold" >Unlimited Images</Navbar.Brand>
+                        <Navbar.Brand><NavLink to="/" className="text-white font-weight-bold text-decoration-none" >Unlimited Images</NavLink></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto">
-                                <Nav.Link href="/home" className="text-white font-weight-bold">Home</Nav.Link>
-                                <Nav.Link href="/images" className="text-white font-weight-bold">Images</Nav.Link>
-                                <Nav.Link href="/videos" className="text-white font-weight-bold">Videos</Nav.Link>
+                                <NavLink to="/home" className="text-white mx-1 font-weight-bold">Home</NavLink>
+                                <NavLink to="/images" className="text-white mx-1 font-weight-bold">Images</NavLink>
+                                <NavLink to="/videos" className="text-white mx-1 font-weight-bold">Videos</NavLink>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
